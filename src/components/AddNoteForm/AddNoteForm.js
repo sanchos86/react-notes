997e5b78f2a8d328596c49da7027bd2225c0addd
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2022-04-15 11:07:56 +0300

    feat: add react-hook-form to AddNoteForm.js

--- src/components/AddNoteForm/AddNoteForm.js
+++ src/components/AddNoteForm/AddNoteForm.js
@@ -1,46 +1,33 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import CategoriesOptions from './CategoriesOptions';
 import categories from '../../constants/categories';
 
 const AddNoteForm = ({ addNote }) => {
-  const [form, setForm] = useState({
-    category: categories.default.value,
-    text: '',
+  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm({
+    defaultValues: {
+      category: categories.default.value,
+      text: '',
+    },
+    mode: 'onChange',
   });
 
   const resetForm = () => {
-    setForm({
-      category: categories.default.value,
-      text: '',
-    });
+    reset();
   };
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    addNote(form);
+  const submitForm = (data) => {
+    addNote(data);
     resetForm();
   }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  }
-
-  const { text, category } = form;
-
   return (
-    <Form onSubmit={submitForm}>
+    <Form onSubmit={handleSubmit(submitForm)}>
       <Form.Group className="mb-3">
         <Form.Label>Category</Form.Label>
         <Form.Select
-          name={'category'}
-          value={category}
-          onChange={handleChange}
+          {...register('category')}
         >
           <CategoriesOptions />
         </Form.Select>
@@ -48,14 +35,21 @@ const AddNoteForm = ({ addNote }) => {
       <Form.Group className="mb-3">
         <Form.Label>Text</Form.Label>
         <Form.Control
-          name={'text'}
-          value={text}
+          {
+            ...register('text', {
+              required: { value: true, message: 'Text is required' },
+              minLength: { value: 3, message: 'Text should be at least 3 characters' }
+            })
+          }
           placeholder="Enter note text"
-          onChange={handleChange}
+          isInvalid={!!errors.text}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.text?.message}
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className={'text-end'}>
-        <Button type={'submit'}>Add note</Button>
+        <Button type={'submit'} disabled={!isValid}>Add note</Button>
       </Form.Group>
     </Form>
   );
